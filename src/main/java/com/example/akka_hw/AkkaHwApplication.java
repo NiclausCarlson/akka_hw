89f988eede9717d7c9706d25ba81fdda45b25a10
NikclausCarlon <you@example.com>
2023-03-05 22:14:18 +0300

    create project and add dependencies

---
+++ src/main/java/com/example/akka_hw/AkkaHwApplication.java
@@ -0,0 +1,13 @@
+package com.example.akka_hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkkaHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkkaHwApplication.class, args);
+    }
+
+}
